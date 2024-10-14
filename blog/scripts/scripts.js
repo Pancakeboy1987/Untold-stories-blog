d816ce062528cd177924aba8ebf9b72a921a4ce8
Akechi Goro <zane41181@gmail.com>
2024-10-14 15:34:35 +0300

    commit

--- blog/scripts/scripts.js
+++ blog/scripts/scripts.js
@@ -1,13 +1,14 @@
-
-const tgButton = document.querySelector('.tg');
+const tgButton = document.querySelector(".tg");
 
 // Обработчик события при наведении
-tgButton.addEventListener('mouseenter', () => {
-    tgButton.style.transition = 'transform 0.3s ease'; // Плавный переход
-    tgButton.style.transform = 'translateY(-5px)'; // Поднимаем кнопку
+tgButton.addEventListener("mouseenter", () => {
+  tgButton.style.transition = "transform 0.3s ease"; // Плавный переход
+  tgButton.style.transform = "translateY(-5px)"; // Поднимаем кнопку
 });
 
 // Обработчик события при уходе курсора
-tgButton.addEventListener('mouseleave', () => {
-    tgButton.style.transform = 'translateY(0)'; // Возвращаем кнопку на место
+tgButton.addEventListener("mouseleave", () => {
+  tgButton.style.transform = "translateY(0)"; // Возвращаем кнопку на место
 });
+
+
